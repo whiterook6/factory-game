2507bf2f6332a9bbcd9f18120939ecbb5f5ea9df
Tim Graboski <whiterook6@gmail.com>
2024-10-23 10:46:49 -0700

    Cleaning up pipe rendering with better functions. Next is intersections and adding input/output points to pipe network

---
+++ src/Pipes.ts
@@ -0,0 +1,189 @@
+import { Framebuffer } from "./Framebuffer";
+
+export type Cell = [number, number];
+
+export enum Heading {
+  LEFT = 0,
+  RIGHT = 1,
+  FORWARD = 2,
+};
+
+export type Segment = "║" | "═" | "╗" | "╝" | "╚" | "╔" | "╣" | "╠" | "╩" | "╦" | "╬";
+
+export enum Direction {
+  NORTH = 0,
+  EAST = 1,
+  SOUTH = 2,
+  WEST = 3,
+};
+
+export type Pipe = [
+  /** the X coordinate of the start */
+  number,
+
+  /** the Y coordinate of the start */
+  number,
+
+  /** the initial facing direction, facing down into the pipe */
+  Direction,
+
+  /** the turns or forwards until the end of the pipe */
+  ...Heading[]
+];
+
+export type Network = Pipe[];
+
+export const getNextCell = ([x, y]: Cell, direction: Direction ): Cell => {
+  switch (direction){
+    case Direction.NORTH:
+      return [x, y - 1];
+    case Direction.EAST:
+      return [x + 1, y];
+    case Direction.SOUTH:
+      return [x, y + 1];
+    case Direction.WEST:
+      return [x - 1, y];
+  }
+}
+
+/**
+ * Given a current direction and a turn (left, right, or forward), what's the resulting direction?
+ */
+export const getNextDirection = (currentDirection: Direction, turn: Heading): Direction => {
+  if (turn === Heading.FORWARD){
+    return currentDirection;
+  }
+
+  switch (currentDirection){
+    case Direction.NORTH:
+      if (turn === Heading.LEFT){
+        return Direction.WEST;
+      } else {
+        return Direction.EAST;
+      }
+    case Direction.EAST:
+      if (turn === Heading.LEFT){
+        return Direction.NORTH;
+      } else {
+        return Direction.SOUTH;
+      }
+    case Direction.SOUTH:
+      if (turn === Heading.LEFT){
+        return Direction.EAST;
+      } else {
+        return Direction.WEST;
+      }
+    case Direction.WEST:
+    default:
+      if (turn === Heading.LEFT){
+        return Direction.SOUTH;
+      } else {
+        return Direction.NORTH;
+      }
+  }
+}
+
+export const getCells = (pipe: Pipe): Cell[] => {
+  if (pipe.length < 4){
+    return [
+      [pipe[0], pipe[1]]
+    ];
+  }
+
+  let currentDirection = pipe[2];
+  let currentCell: Cell = [pipe[0], pipe[1]];
+  return [
+    currentCell,
+    ...pipe.slice(3).map(turn => {
+      const nextDirection = getNextDirection(currentDirection, turn);
+      currentCell = getNextCell(currentCell, nextDirection);
+      return currentCell as Cell;
+    })
+  ]
+};
+
+export const getPipeSegment = (direction: Direction, turn: Heading): Segment => {
+  switch (turn){
+    case Heading.FORWARD:
+      switch (direction){
+        case Direction.NORTH:
+          case Direction.SOUTH:
+          return "║";
+        case Direction.EAST:
+        case Direction.WEST:
+          return "═";
+      }
+    case Heading.LEFT:
+      switch (direction){
+        case Direction.NORTH:
+          return "╗";
+        case Direction.EAST:
+          return "╝";
+        case Direction.SOUTH:
+          return "╚";
+        case Direction.WEST:
+          return "╔";
+      }
+    case Heading.RIGHT:
+    default:
+      switch (direction){
+        case Direction.NORTH:
+          return "╔";
+        case Direction.EAST:
+          return "╗";
+        case Direction.SOUTH:
+          return "╝";
+        case Direction.WEST:
+        default:
+          return "╚";
+      }
+  }
+}
+
+
+
+export class TestPipe {
+  public testPipe: Pipe = [5, 5, Direction.EAST,
+    Heading.FORWARD,
+    Heading.FORWARD,
+    Heading.FORWARD,
+    Heading.FORWARD,
+    Heading.FORWARD,
+    Heading.FORWARD,
+    Heading.LEFT,
+    Heading.FORWARD,
+    Heading.FORWARD,
+    Heading.LEFT,
+    Heading.FORWARD,
+    Heading.FORWARD,
+    Heading.FORWARD,
+    Heading.LEFT,
+    Heading.FORWARD,
+    Heading.FORWARD,
+    Heading.FORWARD,
+    Heading.FORWARD,
+    Heading.RIGHT,
+    Heading.FORWARD,
+    Heading.FORWARD,
+  ];
+  
+  render(framebuffer: Framebuffer){
+    if (this.testPipe.length < 4){
+      framebuffer.write(this.testPipe[0], this.testPipe[1], [
+        [getPipeSegment(this.testPipe[2] /* direction */, Heading.FORWARD), 255, 255, 255, 0, 0, 0]            
+      ]);
+      return;
+    }
+    
+    let currentCell: Cell = [this.testPipe[0], this.testPipe[1]];
+    let currentDirection: Direction = this.testPipe[2];
+    this.testPipe.slice(3).forEach(turn => {
+      const segment = getPipeSegment(currentDirection, turn);
+      framebuffer.write(currentCell[0], currentCell[1], [
+        [segment, 255, 255, 255, 0, 0, 0]
+      ]);
+      currentDirection = getNextDirection(currentDirection, turn);
+      currentCell = getNextCell(currentCell, currentDirection);
+    });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { Connection } from "./Connection";
 import ansi from "ansi";
 import { Framebuffer, RGB, TOKEN } from "./Framebuffer";
 import { Cell, manhattanCost, Pathfinder } from "./Pathfinder";
+import { TestPipe } from "./Pipes";
 
 const run = () => {
   const cursor = ansi(process.stdout);
@@ -24,86 +25,11 @@ const run = () => {
   const terminalHeight = process.stdout.rows;
   const framebuffer = new Framebuffer(terminalWidth, terminalHeight);
 
-  const source = {
-    x: 10,
-    y: 2,
-    width: 20,
-    height: 3,
-  };
-  // get the center of the right side of source
-  const sourceNode: Cell = [
-    source.x + source.width, Math.floor(source.y + source.height / 2),
-  ];
-
-  const destination = {
-    x: 20,
-    y: 8,
-    width: 20,
-    height: 3,
-  };
-  // get the middle of the left edge of the destination
-  const destinationNode: Cell = [
-    destination.x, Math.floor(destination.y + destination.height / 2),
-  ];
-
-  const pathfinder = new Pathfinder();
-  const path = pathfinder.findPath(sourceNode, destinationNode, (from: Cell, to: Cell) => {
-    if (to[0] === destinationNode[0] && to[1] === destinationNode[1]){
-      return true;
-    }
-    for (const box of [source, destination]){
-      const left = box.x;
-      const right = box.x + box.width;
-      const top = box.y;
-      const bottom = box.y + box.height;
-      if (to[0] >= left && to[0] <= right && to[1] >= top && to[1] <= bottom){
-        return false;
-      }
-    }
-    return true;
-  }, manhattanCost);
+  const pipeTest = new TestPipe();
 
   setInterval(() => {
-    const dt = 60 / 1000;
-    
-
-
     framebuffer.clear();
-    // draw a box for source
-    framebuffer.write(source.x, source.y, [
-      ["╔" + "═".repeat(source.width - 2) + "╗", 255, 255, 255, 0, 0, 0]
-    ] as TOKEN[]);
-    
-    for (let y = source.y + 1; y < source.y + source.height - 1; y++){
-      framebuffer.write(source.x, y, [
-        ["║" + " ".repeat(source.width - 2) + "║", 255, 255, 255, 0, 0, 0]
-      ]);
-    }
-    framebuffer.write(source.x, source.y + source.height - 1, [
-      ["╚" + "═".repeat(source.width - 2) + "╝", 255, 255, 255, 0, 0, 0],
-    ] as TOKEN[]);
-
-    // draw a box for destination
-    framebuffer.write(destination.x, destination.y, [
-      ["╔" + "═".repeat(destination.width - 2) + "╗", 255, 255, 255, 0, 0, 0]
-    ] as TOKEN[]);
-    
-    for (let y = destination.y + 1; y < destination.y + destination.height - 1; y++){
-      framebuffer.write(destination.x, y, [
-        ["║" + " ".repeat(destination.width - 2) + "║", 255, 255, 255, 0, 0, 0]
-      ]);
-    }
-    framebuffer.write(destination.x, destination.y + destination.height - 1, [
-      ["╚" + "═".repeat(destination.width - 2) + "╝", 255, 255, 255, 0, 0, 0],
-    ] as TOKEN[]);
-
-    // draw a * for each cell in the path
-    for (const cell of path){
-      framebuffer.write(cell[0], cell[1], [
-        ["*", 255, 255, 255, 0, 0, 0],
-      ] as TOKEN[]);
-    }
-
+    pipeTest.render(framebuffer);
     framebuffer.render(cursor);
   }, 1000 / 60);
 };
